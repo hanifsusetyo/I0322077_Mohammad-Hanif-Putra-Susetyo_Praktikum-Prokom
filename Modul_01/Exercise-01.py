293c22a062b93b938d31fed462be91aebb1b4b6e
hanifsusetyo <hanifputrasusetyo@student.uns.ac.id>
2023-03-14 01:03:24 +0700

    commit exercise 1 prakprokom

---
+++ Modul_01/Exercise-01.py
@@ -0,0 +1,8 @@
+#exercise-01 Praktikum Prokom
+
+print("Mohammad Hanif Putra Susetyo")
+print("I0322077")
+print("Kelas C")
+print()
+print("Nama saya Mohammad Hanif Putra Susetyo, biasa dipanggil Hanif, dan saya berasal dari Ciakrang Kab. Bekasi.")
+print("Saya sekarang tinggal di Kota Surakarta, dikarenakan saya sedang menjalani kuliah di Universitas Sebelas Maret, lebih tepatnya di jurursan Teknik Industri.")
